bcf855deb0e3044979c21f1063a547660ed3bde7
Sidharth J Dev <sid.j.dev@gmail.com>
2024-05-28 21:24:54 +0530

    Initial Commit

---
+++ TechieButler/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TechieButler
+//
+//  Created by Sidharth J Dev on 28/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TechieButler/TechieButlerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TechieButlerApp.swift
+//  TechieButler
+//
+//  Created by Sidharth J Dev on 28/05/24.
+//
+
+import SwiftUI
+
+@main
+struct TechieButlerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
